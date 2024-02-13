8e6b8f64149c338ac91d359c34066673e00739c2
tora180405 <toradigda@gmail.com>
2024-02-13 21:08:02 +0700

    add file

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>About</title>
+</head>
+<body>
+    <h1>About Me</h1>
+    <p>Name : Tora Dogda Kristiawan</p>
+    <p>Emaail : toradigda@gmail.com</p>
+    <img src="img/test.jpg" alt="tora digda kristiawan" style="width: 500px">
+
+    <script src="js/script.js"></script>
+</body>
+</html>
