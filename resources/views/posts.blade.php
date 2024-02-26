e2af1915810586b4507ebce55b08a24928d3a5e2
tora180405 <toradigda@gmail.com>
2024-02-26 20:38:20 +0700

    rededign ui

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,6 +5,15 @@
 
 <h1 class="mb-5">{{ $title }}</h1>
 
+<div class="card mb-3">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+
     @foreach ($post as $post)
         <article class="mb-5 border-bottom pb-4">
             <h2>
